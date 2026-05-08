01170ff267e4e9fe1814092b5e2389d487a0bc85
ChoCoMaMiRe27 <bintianjiecai3@gmail.com>
2026-05-08 11:51:08 +0900

    ’posts#index’のビュー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,4 @@ class PostsController < ApplicationController
   def index
   end
 
-  def new
-  end
-
-   def create
-     Post.create(content: params[:content])
-      redirect_to "/posts"
-  end
 end
